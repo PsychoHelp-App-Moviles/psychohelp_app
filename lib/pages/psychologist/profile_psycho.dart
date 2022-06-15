a69361b3107e363e68201f56e33bba8e10c96a27
JosePainP <67244047+ThePain99@users.noreply.github.com>
2022-06-14 21:26:52 -0500

    add circular

--- lib/pages/psychologist/profile_psycho.dart
+++ lib/pages/psychologist/profile_psycho.dart
@@ -10,6 +10,8 @@ class Profile_psycho extends StatefulWidget {
   State<Profile_psycho> createState() => _Profile_psychoState();
 }
 
+bool isLoading = true;
+
 class _Profile_psychoState extends State<Profile_psycho> {
   HttpHelper httpHelper = HttpHelper();
   Psychologist psychologist = new Psychologist(
@@ -68,89 +70,97 @@ class _Profile_psychoState extends State<Profile_psycho> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Profile"),
+    if (psychologist.name == "") {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
-        body: Container(
-            margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
-            width: MediaQuery.of(context).size.width,
-            child: Align(
-              alignment: Alignment.center,
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(psychologist.img),
-                      radius: 75.0,
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          psychologist.name,
-                          style: TextStyle(fontSize: 30.0),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Column(
-                      children: [
-                        Card(
-                          child: Container(
-                            padding: EdgeInsets.all(20.0),
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: Column(
-                              children: [
-                                Text("Email: " + psychologist.email,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Phone: " + psychologist.phone,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Birthday: " + psychologist.birthday,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Gender: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Specialization: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Formation: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("CMP: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("Session type: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                                SizedBox(height: 10),
-                                Text("About: " + psychologist.gender,
-                                    style: TextStyle(fontSize: 20.0)),
-                              ],
+      );
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Profile"),
+          ),
+          body: Container(
+              margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
+              width: MediaQuery.of(context).size.width,
+              child: Align(
+                alignment: Alignment.center,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(psychologist.img),
+                        radius: 75.0,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            psychologist.name,
+                            style: TextStyle(fontSize: 30.0),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Column(
+                        children: [
+                          Card(
+                            child: Container(
+                              padding: EdgeInsets.all(20.0),
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              child: Column(
+                                children: [
+                                  Text("Email: " + psychologist.email,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Phone: " + psychologist.phone,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Birthday: " + psychologist.birthday,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Gender: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Specialization: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Formation: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("CMP: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("Session type: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                  SizedBox(height: 10),
+                                  Text("About: " + psychologist.gender,
+                                      style: TextStyle(fontSize: 20.0)),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            _navigateAndDisplaySelection(context);
-                          },
-                          icon: Icon(
-                            Icons.edit_outlined,
-                            size: 25,
-                          ),
-                          label: Text("Edit profile",
-                              style: TextStyle(fontSize: 20.0)),
-                        )
-                      ],
-                    )
-                  ],
+                          SizedBox(height: 20),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              _navigateAndDisplaySelection(context);
+                            },
+                            icon: Icon(
+                              Icons.edit_outlined,
+                              size: 25,
+                            ),
+                            label: Text("Edit profile",
+                                style: TextStyle(fontSize: 20.0)),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )));
+              )));
+    }
   }
 }
